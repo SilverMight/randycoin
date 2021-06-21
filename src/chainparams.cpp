8c1dbc6566dd69e7d6626e68b9f5a0c332b7f32f
SilverMight <joeyzee1234@gmail.com>
2021-06-20 21:38:56 -0400

    dnsseeds and seednodes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,10 +120,10 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed-a.randycoin.loshan.co.uk");
-        vSeeds.emplace_back("dnsseed.thrasher.io");
-        vSeeds.emplace_back("dnsseed.randycointools.com");
-        vSeeds.emplace_back("dnsseed.randycoinpool.org");
+        //vSeeds.emplace_back("seed-a.randycoin.loshan.co.uk");
+        //vSeeds.emplace_back("dnsseed.thrasher.io");
+        //vSeeds.emplace_back("dnsseed.randycointools.com");
+        //vSeeds.emplace_back("dnsseed.randycoinpool.org");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,60);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
